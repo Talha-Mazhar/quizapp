eccd8c8f09cf74dc91c41b9f52c73d84ed8cdf4d
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-02-15 14:16:17 +0500

    Listing quizes

---
+++ app/src/main/java/com/example/quizapp/QuizesList.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizes_list);
+    }
+}
